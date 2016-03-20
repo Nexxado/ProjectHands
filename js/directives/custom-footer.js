10e89c8cd333472483498df076b2fc29bb55d477
Netanel Draiman <ndraiman@gmail.com>
2016-03-20 14:50:17 +0200

    added replace true to directives

--- js/directives/custom-footer.js
+++ js/directives/custom-footer.js
@@ -3,6 +3,7 @@ angular.module('ProjectHands')
 .directive('customFooter', function() {
     return {
         restrict: 'E',
+        replace: true,
         templateUrl: 'templates/directives/custom-footer.html',
         controller: function($scope) {
             
--- js/directives/toolbar.js
+++ js/directives/toolbar.js
@@ -3,6 +3,7 @@ angular.module('ProjectHands')
 .directive('toolbar', function () {
     return {
         restrict: 'E',
+        replace: true,
         templateUrl: 'templates/directives/toolbar.html',
         controller: function ($scope) {
             $scope.inputUsername = '';
