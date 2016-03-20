f782cb548fac2f3b89eeda4b07649fd97d198814
Netanel Draiman <ndraiman@gmail.com>
2016-03-20 17:51:35 +0200

    fixed chat routes and profile template

--- js/routes.js
+++ js/routes.js
@@ -1,28 +1,28 @@
 angular.module('ProjectHands')
 
 .config(function ($stateProvider, $urlRouterProvider) {
-    
+
     $urlRouterProvider.otherwise('/home');
-    
+
     $stateProvider.state('home', {
         url: '/home',
         templateUrl: 'templates/home.html'
-        
-    });
+
+    })
     
-        $stateProvider.state('profile', {
+    .state('profile', {
         url: '/profile',
         templateUrl: 'templates/profile.html'
-        
-    });
-    
-      $stateProvider.state('chat', {
+
+    })
+
+    .state('chat', {
         url: '/chat',
         templateUrl: 'templates/chat.html'
-        
-     });
-    
-    
-    
-    
+
+    });
+
+
+
+
 });
