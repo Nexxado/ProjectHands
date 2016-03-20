1ee31ce99ee8dfc5aa630f4ff23a143a52fb957d
Netanel Draiman <ndraiman@gmail.com>
2016-03-20 13:47:02 +0200

    attempt to center ad

--- js/directives/custom-footer.js
+++ js/directives/custom-footer.js
@@ -3,6 +3,9 @@ angular.module('ProjectHands')
 .directive('customFooter', function() {
     return {
         restrict: 'E',
-        templateUrl: 'templates/directives/custom-footer.html'
+        templateUrl: 'templates/directives/custom-footer.html',
+        controller: function($scope) {
+            
+        }
     };
 });
