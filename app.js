e0e5cc74f441cc30a8947b2af0ac343c07092d88
Netanel Draiman <ndraiman@gmail.com>
2016-04-01 00:00:50 +0300

    added basic mongo framework

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+
+var mongoUtil = require('./mongoUtils');
+mongoUtil.connect();
+
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/client')); //Static route for client side
 app.use('/vendor', express.static(__dirname + '/node_modules/')); //Static Route for node_modules
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening on port ' + port + '\n');
 });
---
+++ mongoUtils.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+var config = require('./config.json');
+var _db;
+
+
+
+module.exports = {
+    connect() {
+        MongoClient.connect(config.mongoDBUrl, function (err, db) {
+            if (err) {
+                console.log("Error connecting to Mongo: ", err);
+                process.exit(1);
+            }
+            console.log("connected to Mongo");
+            
+            _db = db;
+        });
+    },
+    renovations() {
+        return _db.collection('renovations');
+    },
+    users() {
+        return _db.collection('users');
+    },
+    chat() {
+        return _db.collection('chat');
+    }
+};
