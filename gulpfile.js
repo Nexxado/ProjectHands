dfd3ed6496dc958220e5fa2f86830d3df5a4d773
Netanel Draiman <ndraiman@gmail.com>
2016-04-01 17:08:03 +0300

    added jshint gulp task, converted links to badges in README

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('jshint', function() {
+    
+    return gulp.src('./client/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('css-autoprefixer', function() {
+    
+    return gulp.src('./client/css/*.css', { base: './'})
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('./client/js/**/*.js', ['jshint']);
+});
+
+gulp.task('default', ['watch']);
