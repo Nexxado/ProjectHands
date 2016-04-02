2062d944a4a1a09467d80eb542843670e2f07724
Netanel Draiman <ndraiman@gmail.com>
2016-04-02 20:08:17 +0300

    added linux script for db-seed

---
+++ db-seed/db-seed.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mongoimport --db hands --collection users --type json --file ./users-seed.json --jsonArray --drop
+mongoimport --db hands --collection chats --type json --file ./chats-seed.json --jsonArray --drop
+mongoimport --db hands --collection renovations --type json --file ./renovations-seed.json --jsonArray --drop
+mongoimport --db hands --collection teams --type json --file ./teams-seed.json --jsonArray --drop
