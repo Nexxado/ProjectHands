274fe524553a8c4f971ccb6fa0ba8df8d575d49b
Ruby <reuven.lichtenstein@gmail.com>
2016-04-07 00:32:18 +0300

    working on chat and members grid-items

--- client/js/controllers/renovationDashboardController.js
+++ client/js/controllers/renovationDashboardController.js
@@ -2,5 +2,92 @@
  * Created by Ruby on 4/4/2016.
  */
 angular.module('ProjectHands')
-    .controller('renovationDashboardController', function ($scope){});
+    .controller('renovationDashboardController', function ($scope){
+
+        $scope.members = [
+            {
+                name: 'ruby',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'david',
+                role: 'manager',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'dani',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'mike',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'ruby',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'david',
+                role: 'manager',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'dani',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'mike',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },{
+                name: 'ruby',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'david',
+                role: 'manager',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'dani',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            },
+            {
+                name: 'mike',
+                role: 'volunteer',
+                imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+            }]
+
+
+        $scope.messages = [
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'},
+            {content: 'abcd'}
+        ]
+    });
 
---
+++ client/js/directives/renovation.js
@@ -0,0 +1,84 @@
+angular.module('ProjectHands')
+
+.directive('renovation', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'templates/directives/renovation.html',
+        controller: function($scope) {
+            $scope.members = [
+                {
+                    name: 'ruby',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'david',
+                    role: 'manager',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'dani',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'mike',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'ruby',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'david',
+                    role: 'manager',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'dani',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'mike',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },{
+                    name: 'ruby',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'david',
+                    role: 'manager',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'dani',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                },
+                {
+                    name: 'mike',
+                    role: 'volunteer',
+                    imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
+
+                }]
+            
+        }
+    };
+});
