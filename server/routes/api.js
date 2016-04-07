5387eff362326d50e03a4f01fabcf239d15466b0
Netanel Draiman <ndraiman@gmail.com>
2016-04-07 18:12:17 +0300

    removed redundant methods

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,6 @@
 var router = require('express').Router();
 var mongoUtils = require('../mongoUtils');
 
-router.get('/chat', function (req, res) {
-    res.json(mongoUtils.chats().find());
-});
-
-// router.post('/chat/:room&:message', function (req, res) {
-//     console.log('POST req = ', req);
-//     console.log('POST params = ', req.params);
-//     // mongoUtils.chats().update({"_id": req.params.room}, {$push: req.params.message});
-//     res.sendStatus(200);
-// });
 
 router.get('/chat/:room', function (req, res) {
     mongoUtils.chats().find({"_id": req.params.room})
