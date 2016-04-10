9cd45ec5d40381034a75e980f815800e6790ee5b
RubyLichtenstein <reuven.lichtenstein@gmail.com>
2016-04-09 21:41:30 -0700

    create task dialog

--- client/js/controllers/renovationDashboardController.js
+++ client/js/controllers/renovationDashboardController.js
@@ -2,7 +2,7 @@
  * Created by Ruby on 4/4/2016.
  */
 angular.module('ProjectHands')
-    .controller('renovationDashboardController', function ($scope) {
+    .controller('renovationDashboardController', function ($scope, $mdDialog) {
 
         $scope.members = [
             {
@@ -79,7 +79,7 @@ angular.module('ProjectHands')
 
         $scope.messages = [
             {
-                content: 'renovationDashboardController renovationDashboardController renovationDashboardController renovationDashboardController renovationDashboardController  ',
+                content: 'renovationDashboardControllerrenovationDashboardController renovationDashboardController renovationDashboardController renovationDashboardController  ',
                 sender: 'ruby',
                 role: 'manager',
                 imageUrl: 'https://scontent-cdg2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12523086_575552755928478_5791044670543493931_n.jpg?oh=fe49cfc91c579a052dec8158b72ab7c1&oe=5780F28B'
@@ -120,26 +120,10 @@ angular.module('ProjectHands')
                 creator: 'a',
                 description: 'b',
                 created_at: 'c',
+                priority: 'high',
+                due_date: 'dd',
                 executors: []
-            },
-            {
-                creator: 'a',
-                description: 'b',
-                created_at: 'c',
-                executors: []
-            },
-            {
-                creator: 'a',
-                description: 'b',
-                created_at: 'c',
-                executors: []
-            },
-            {
-                creator: 'a',
-                description: 'b',
-                created_at: 'c',
-                executors: []
-            },
+            }
         ];
 
         $scope.tools = [
@@ -161,7 +145,66 @@ angular.module('ProjectHands')
 
 
         ];
-        
+
+        $scope.inforamtion = {
+            description: 'abcbcbcbcb',
+            address: 'acscscscsc',
+            phone: '050-555-555',
+            date: 'acacacacac'
+        };
+
+        $scope.showTabDialog = function (ev) {
+            $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: '/templates/dialogs/create-task-dialog.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true
+                })
+                .then(function (answer) {
+                    // $scope.status = answer;
+                    $scope.tasks.push(answer);
+                }, function () {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+        };
+
+
     });
+function DialogController($scope, $mdDialog) {
+
+    var task = {
+        creator: 'a',
+        description: 'b',
+        created_at: 'c',
+        priority: 'high',
+        due_date: 'dd',
+        executors: []
+    };
+
+    var vm = this;
+    $scope.announceClick = function(priority) {
+
+        task.priority = priority;
+
+        $scope.task.priority = priority;
+    };
+
+    $scope.myDate = new Date();
+
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    $scope.answer = function (answer) {
+
+        task.due_date = $scope.myDate;
+        task.description = $scope.task.description ;
+
+        $mdDialog.hide(task);
+    };
+}
 
 
