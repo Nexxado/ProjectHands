f1e9e1b44dc4fa714db7aaf6cfc432a0ce635d58
Netanel Draiman <ndraiman@gmail.com>
2016-04-16 16:07:04 +0300

    added decorator to exceptionHandler

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,22 @@
 angular.module('ProjectHands', ['ngResource', 'ngAria', 'ngAnimate', 'ngMessages', 'ngMaterial', 'ui.router', 'ct.ui.router.extras', 'gridster', 'ui.calendar'])
 
 
-.config(function ($mdThemingProvider) {
+.config(function ($mdThemingProvider, $provide) {
     $mdThemingProvider.theme('default')
         .primaryPalette('blue')
         .accentPalette('orange');
+
+    $provide.decorator('$exceptionHandler', function($delegate) {
+        return function(exception, cause) {
+
+            exception.message = exception.message +
+                '\nCaused by: ' + cause +
+                '\nOrigin: ' + exception.fileName + ':' + exception.lineNumber + ':' + exception.columnNumber +
+                '\n\nStacktrace:';
+
+            $delegate(exception, cause);
+        };
+    });
 })
 
 .constant('COLLECTIONS', {
