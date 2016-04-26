52c4888f86090fe7f2707e1025ec9839386603a1
Netanel Draiman <ndraiman@gmail.com>
2016-04-27 00:41:09 +0300

    SignUp: added function to check if ID is legal

--- client/modules/auth/controllers/signupController.js
+++ client/modules/auth/controllers/signupController.js
@@ -15,6 +15,11 @@ angular.module('ProjectHands.auth')
         if ($scope.SignupForm.$invalid)
             return;
 
+        if(!isLegalID($scope.user._id)) {
+            $scope.makeToast('תעודת זהות אינה חוקית', 'md-content', 'bottom right');
+            return;
+        }
+
         AuthService.signup($scope.user).$promise
             .then(function (data) {
                 console.log('data', data);
@@ -24,4 +29,43 @@ angular.module('ProjectHands.auth')
                 $scope.makeToast(error.data.errMessage, 'md-content', 'bottom right');
             });
     };
+
+
+    /**
+     * Checks if a 9 Digit ID is a legal Israeli ID
+     * @param   {string}  id : 9 digit ID number as string
+     * @returns {boolean} : true if legal ID, otherwise false
+     * Algorithm:
+     * 1) Multiply each ID digit with each corrsponding ID of the check number
+     * 2) For any result with 2 digits, add the digits together
+     * 3) Sum all the resulting numbers, if the sum is divisble by 10, its legal.
+     */
+    function isLegalID(id) {
+
+        var check = "121212121";
+        var array = [];
+        var sum = 0;
+
+        for(var i =  0; i < id.length; i++) {
+            var result = parseInt(id.charAt(i)) * parseInt(check.charAt(i));
+            array.push(result);
+        }
+
+        for(var i = 0; i < array.length; i++) {
+            if(array[i] >= 10) {
+                var numStr = array[i].toString();
+                var result = parseInt(numStr.charAt(0)) + parseInt(numStr.charAt(1));
+                array[i] = result;
+            }
+        }
+
+        for(var i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        if(sum % 10)
+            return false;
+
+        return true;
+    }
 });
