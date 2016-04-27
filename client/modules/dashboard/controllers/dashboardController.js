883747607d314306322964d27f77d6dff5bde589
Dan Draiman <DDraiman1990@gmail.com>
2016-04-27 21:48:57 +0300

    added more functions to renovation-page. still WIP

--- client/modules/dashboard/controllers/dashboardController.js
+++ client/modules/dashboard/controllers/dashboardController.js
@@ -3,7 +3,7 @@ angular.module('ProjectHands.dashboard')
 .controller('DashboardController', function ($scope) {
 
     $scope.editLayoutEnabled = false;
-    
+
     /*Widgets properties*/
     $scope.widgets = [
         {
@@ -77,86 +77,85 @@ angular.module('ProjectHands.dashboard')
             active: false
         }
     ];
-    
+
     /*Grister Settings*/
     $scope.gridsterOpts = {
 
-		columns: 6, // the width of the grid, in columns
-		pushing: false, // whether to push other items out of the way on move or resize
-		floating: false, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
-		swapping: true, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
-		width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
-		colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
-		rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
-		margins: [10, 10], // the pixel distance between each widget
-		outerMargin: true, // whether margins apply to outer edges of the grid
-		isMobile: false, // stacks the grid items if true
-		mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
-		mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
-		minColumns: 1, // the minimum columns the grid must have
-		minRows: 2, // the minimum height of the grid, in rows
-		maxRows: 100,
-		defaultSizeX: 2, // the default width of a gridster item, if not specifed
-		defaultSizeY: 1, // the default height of a gridster item, if not specified
-		minSizeX: 2, // minimum column width of an item
-		maxSizeX: null, // maximum column width of an item
-		minSizeY: 3, // minumum row height of an item
-		maxSizeY: null, // maximum row height of an item 
-		resizable: {
-			enabled: false,
-			start: function (event, $element, widget) {
-			}, // optional callback fired when resize is started,
-			resize: function (event, $element, widget) {
-			}, // optional callback fired when item is resized,
-			stop: function (event, $element, widget) {
-				} // optional callback fired when item is finished resizing
-		},
-		draggable: {
-			enabled: false, // whether dragging items is supported
-//			handle: '.my-class', // optional selector for resize handle
-			start: function (event, $element, widget) {
-			}, // optional callback fired when drag is started,
-			drag: function (event, $element, widget) {
+        columns: 6, // the width of the grid, in columns
+        pushing: false, // whether to push other items out of the way on move or resize
+        floating: false, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
+        swapping: true, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
+        width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
+        colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
+        rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+        margins: [10, 10], // the pixel distance between each widget
+        outerMargin: true, // whether margins apply to outer edges of the grid
+        isMobile: false, // stacks the grid items if true
+        mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
+        mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
+        minColumns: 1, // the minimum columns the grid must have
+        minRows: 2, // the minimum height of the grid, in rows
+        maxRows: 100,
+        defaultSizeX: 2, // the default width of a gridster item, if not specifed
+        defaultSizeY: 1, // the default height of a gridster item, if not specified
+        minSizeX: 2, // minimum column width of an item
+        maxSizeX: null, // maximum column width of an item
+        minSizeY: 3, // minumum row height of an item
+        maxSizeY: null, // maximum row height of an item 
+        resizable: {
+            enabled: false,
+            start: function (event, $element, widget) {}, // optional callback fired when resize is started,
+            resize: function (event, $element, widget) {}, // optional callback fired when item is resized,
+            stop: function (event, $element, widget) {} // optional callback fired when item is finished resizing
+        },
+        draggable: {
+            enabled: false, // whether dragging items is supported
+            //			handle: '.my-class', // optional selector for resize handle
+            start: function (event, $element, widget) {}, // optional callback fired when drag is started,
+            drag: function (event, $element, widget) {
 
-			}, // optional callback fired when item is moved,
-			stop: function (event, $element, widget) {
-				} // optional callback fired when item is finished dragging
-		}
-	};
+            }, // optional callback fired when item is moved,
+            stop: function (event, $element, widget) {} // optional callback fired when item is finished dragging
+        }
+    };
 
-	
+
     /*Gridster Functions*/
-	$scope.enableEditLayout = function () {
-		$scope.editLayoutEnabled = true;
-		$scope.gridsterOpts.draggable.enabled = true;
-		$scope.gridsterOpts.resizable.enabled = true;
-		$scope.gridsterOpts.pushing = true;
-		$scope.gridsterOpts.floating = true;
-		$scope.gridsterOpts.swapping = false;
-	};
-	$scope.disableEditLayout = function () {
-		$scope.editLayoutEnabled = false;
-		$scope.gridsterOpts.draggable.enabled = false;
-		$scope.gridsterOpts.resizable.enabled = false;
-		$scope.gridsterOpts.pushing = false;
-		$scope.gridsterOpts.floating = false;
-		$scope.gridsterOpts.swapping = true;
-	};
-	$scope.toggleIsMobile = function () {
-		$scope.gridsterOpts.isMobile = !$scope.gridsterOpts.isMobile;
-	};
-	$scope.toggleMobileModeEnabled = function () {
-		$scope.gridsterOpts.mobileModeEnabled = !$scope.gridsterOpts.mobileModeEnabled;
-	};
-	$scope.$on('gridster-mobile-changed', function (gridster) {
-	});
-	$scope.$on('gridster-draggable-changed', function (gridster) {
-	});
-    
+    $scope.enableEditLayout = function () {
+        $scope.editLayoutEnabled = true;
+        $scope.gridsterOpts.draggable.enabled = true;
+        $scope.gridsterOpts.resizable.enabled = true;
+        $scope.gridsterOpts.pushing = true;
+        $scope.gridsterOpts.floating = true;
+        $scope.gridsterOpts.swapping = false;
+    };
+    $scope.disableEditLayout = function () {
+        $scope.editLayoutEnabled = false;
+        $scope.gridsterOpts.draggable.enabled = false;
+        $scope.gridsterOpts.resizable.enabled = false;
+        $scope.gridsterOpts.pushing = false;
+        $scope.gridsterOpts.floating = false;
+        $scope.gridsterOpts.swapping = true;
+    };
+    $scope.toggleIsMobile = function () {
+        $scope.gridsterOpts.isMobile = !$scope.gridsterOpts.isMobile;
+    };
+    $scope.toggleMobileModeEnabled = function () {
+        $scope.gridsterOpts.mobileModeEnabled = !$scope.gridsterOpts.mobileModeEnabled;
+    };
+    $scope.$on('gridster-mobile-changed', function (gridster) {});
+    $scope.$on('gridster-draggable-changed', function (gridster) {});
+
     /* Mock Objects creations: */
     $scope.myID = "123456197";
-   
+
     $scope.rooms = ["General", "Reno1", "Reno2"];
+    $scope.renovationRooms = ["a"];
+    $scope.addRenoRoom = function (roomName) {
+        $scope.renovationRooms = [];
+        $scope.renovationRooms.push(roomName);
+    };
+
     $scope.tools = [
         {
             name: "פטיש",
@@ -384,46 +383,183 @@ angular.module('ProjectHands.dashboard')
     ];
 
     $scope.address = {
-        city:"",
-        street:"",
+        city: "",
+        street: "",
         houseNum: ""
     };
-    
+
     $scope.renovations = [
         {
             address: {
-                city:"מודיעין",
-                street:"חטיבת הצנחנים",
-                houseNum: "2"  
+                city: "מודיעין",
+                street: "חטיבת הצנחנים",
+                houseNum: "2"
             },
-            team: $scope.teams[0]
+            team: $scope.teams[0],
+            toolsNeeded: [
+                {
+                    name: "פטיש",
+                    quantity: 2,
+                    comment: "peter piper picked a pack of pickled peppers"
+		          },
+                {
+                    name: "מקדחה",
+                    quantity: 1,
+                    comment: "peter piper picked a pack of pickled peppers"
+		          },
+                {
+                    name: "ברגים",
+                    quantity: 1230,
+                    comment: "peter piper picked a pack of pickled peppers"
+		          }
+            ],
+            tasks: [
+                {
+                    title: "לשלשל על הרצפה",
+                    description: "זה חשוב מאוד",
+                    deadline: new Date(2020,1,1),
+//                    assignee: $scope.getMemberName($scope.renovations[0].team.members_id[0])
+                },
+                {
+                    title: "שגדדשג על הרצפה",
+                    description: "שדגשדגשדגשדגשדגדשג",
+                    deadline: new Date(2018,2,2),
+//                    assignee: $scope.getMemberName($scope.renovations[0].team.members_id[1])
+                }
+            ]
         },
         {
             address: {
-                city:"חירייה",
-                street:"השחום",
-                houseNum: "5"  
+                city: "חירייה",
+                street: "השחום",
+                houseNum: "5"
             },
-            team: $scope.teams[1]
+            team: $scope.teams[1],
+            toolsNeeded: [
+
+                {
+                    name: "מסור",
+                    quantity: 3,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "קאטר",
+                    quantity: 1,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "דבק",
+                    quantity: 12,
+                    comment: "peter piper picked a pack of pickled peppers"
+		}
+            ],
+            tasks: [
+                {
+                    title: "לשלשל על הרצפה",
+                    description: "זה חשוב מאוד",
+                    deadline: new Date(1/1/2020),
+//                    assignee: $scope.getMemberName($scope.renovations[1].team.members_id[0])
+                },
+                {
+                    title: "שגדדשג על הרצפה",
+                    description: "שדגשדגשדגשדגשדגדשג",
+                    deadline: new Date(2/2/2018),
+//                    assignee: $scope.getMemberName($scope.renovations[1].team.members_id[1])
+                }
+            ]
         },
         {
             address: {
-                city:"ירושלים",
-                street:"הדביל",
-                houseNum: "21"  
+                city: "ירושלים",
+                street: "הדביל",
+                houseNum: "21"
             },
-            team: $scope.teams[0]
+            team: $scope.teams[0],
+            toolsNeeded: [
+                {
+                    name: "פטיש",
+                    quantity: 2,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+
+                {
+                    name: "מסור",
+                    quantity: 3,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "דבק",
+                    quantity: 12,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "חול",
+                    quantity: 4,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "מברג",
+                    quantity: 2,
+                    comment: "peter piper picked a pack of pickled peppers"
+		}
+
+            ],
+            tasks: [
+                {
+                    title: "לשלשל על הרצפה",
+                    description: "זה חשוב מאוד",
+                    deadline: new Date(1/1/2020),
+//                    assignee: $scope.getMemberName($scope.renovations[2].team.members_id[0])
+                },
+                {
+                    title: "שגדדשג על הרצפה",
+                    description: "שדגשדגשדגשדגשדגדשג",
+                    deadline: new Date(2/2/2018),
+//                    assignee: $scope.getMemberName($scope.renovations[2].team.members_id[1])
+                }
+            ]
         },
         {
             address: {
-                city:"קשקבל",
-                street:"הגבינות",
-                houseNum: "65"  
+                city: "קשקבל",
+                street: "הגבינות",
+                houseNum: "65"
             },
-            team: $scope.teams[1]
+            team: $scope.teams[1],
+            toolsNeeded: [
+                {
+                    name: "דבק",
+                    quantity: 12,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "חול",
+                    quantity: 4,
+                    comment: "peter piper picked a pack of pickled peppers"
+		},
+                {
+                    name: "מברג",
+                    quantity: 2,
+                    comment: "peter piper picked a pack of pickled peppers"
+		}
+            ],
+            tasks: [
+                {
+                    title: "לשלשל על הרצפה",
+                    description: "זה חשוב מאוד",
+                    deadline: new Date(1/1/2020),
+//                    assignee: $scope.getMemberName($scope.renovations[3].team.members_id[0])
+                },
+                {
+                    title: "שגדדשג על הרצפה",
+                    description: "שדגשדגשדגשדגשדגדשג",
+                    deadline: new Date(2/2/2018),
+//                    assignee: $scope.getMemberName($scope.renovations[3].team.members_id[1])
+                }
+            ]
         }
     ];
-    
+
     $scope.messages = [
         {
             id: "ren1_chat",
@@ -458,9 +594,9 @@ angular.module('ProjectHands.dashboard')
     ];
 
     //Mock functions
-//    $scope.getRenovationByID(renoId){
-//        
-//    }
+    //    $scope.getRenovationByID(renoId){
+    //        
+    //    }
     $scope.getMemberName = function (id) {
         var i;
         for (i in $scope.users) {
@@ -557,6 +693,17 @@ angular.module('ProjectHands.dashboard')
         }
 
         return "No user found!";
+    };
+    
+    $scope.getRenovation = function(city, street, houseNum){
+        var i;
+        for (i in $scope.renovations) {
+            if ($scope.renovations[i].address.city == city && $scope.renovations[i].address.street == street && $scope.renovations[i].address.houseNum == houseNum) {
+                return $scope.renovations[i];
+            }
+        }
+
+        return "No renovation found!";
     };
     /*End of Mock Object Creations*/
 });
