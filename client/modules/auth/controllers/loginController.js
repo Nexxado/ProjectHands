c687412bb5464ab59326834ef5fac052963fb9cd
Netanel Draiman <ndraiman@gmail.com>
2016-05-03 19:59:07 +0300

    oauth login is now done via popup

--- client/modules/auth/controllers/loginController.js
+++ client/modules/auth/controllers/loginController.js
@@ -1,6 +1,6 @@
 angular.module('ProjectHands.auth')
 
-.controller('LoginController', function ($scope, $rootScope, AuthService, AUTH_EVENTS) {
+.controller('LoginController', function ($scope, $rootScope, AuthService, AUTH_EVENTS, SessionService, $window) {
 
     var toastAnchor = '#loginToastsAnchor';
 
@@ -17,7 +17,7 @@ angular.module('ProjectHands.auth')
         AuthService.login($scope.email, $scope.password, $scope.rememberMe)
             .then(function (data) {
                 console.log('auth data', data);
-                $rootScope.$broadcast(AUTH_EVENTS.loginSuccess, {userName: data.name, role: data.role});
+                SessionService.getSession();
                 $scope.email = '';
                 $scope.password = '';
                 $scope.LoginForm.$setPristine();
@@ -28,7 +28,18 @@ angular.module('ProjectHands.auth')
                 $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
                 $rootScope.makeToast('האימייל או הסיסמה אינם נכונים', toastAnchor, 'bottom right');
             });
+    };
 
+    $scope.loginGoogle = function() {
+        $window.open('/api/auth/google','',' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no');
+    };
 
+    $scope.loginFacebook = function() {
+        $window.open('/api/auth/facebook','',' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no');
+    };
+
+    $window.loginCallBack = function() {
+        console.log('loginCallBack');
+        SessionService.getSession();
     };
 });
--- client/modules/auth/services/session.js
+++ client/modules/auth/services/session.js
@@ -1,6 +1,6 @@
 angular.module('ProjectHands.auth')
 
-.service('SessionService', function ($rootScope, AuthService) {
+.service('SessionService', function ($rootScope, AuthService, AUTH_EVENTS) {
 
     this.clearSession = function () {
         $rootScope.isLoggedIn = false;
@@ -11,12 +11,15 @@ angular.module('ProjectHands.auth')
     this.getSession = function () {
         AuthService.isLoggedIn().$promise
             .then(function(result) {
+                console.log('getSession result', result);
+                $rootScope.$broadcast(AUTH_EVENTS.loginSuccess, {userName: result.name, role: result.role});
                 $rootScope.isLoggedIn = true;
                 $rootScope.userName = result.name;
                 $rootScope.userEmail = result.email;
             })
             .catch(function(error) {
                 console.log('isLoggedIn error', error);
+//                $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
             });
     };
 });
