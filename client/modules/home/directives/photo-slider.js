aa688e34a0acabcb31ae98691839608f7d53bd23
RubyLichtenstein <reuven.lichtenstein@gmail.com>
2016-05-07 14:14:34 +0300

    fixing photo slider

--- client/modules/home/directives/photo-slider.js
+++ client/modules/home/directives/photo-slider.js
@@ -8,10 +8,16 @@ angular.module('ProjectHands.home')
             controller: function ($scope) {
 
                 $scope.slides = [
-                    {image: 'assets/img/home/jobs1.jpg', description: 'Image 00'},
-                    {image: 'assets/img/home/jobs2.jpg', description: 'Image 01'},
-                    {image: 'assets/img/home/jobs3.jpg', description: 'Image 02'},
-                    {image: 'assets/img/home/jobs5.jpg', description: 'Image 04'}
+                    // {image: 'assets/img/home/jobs1.jpg', description: 'Image 00'},
+                    // {image: 'assets/img/home/jobs2.jpg', description: 'Image 01'},
+                    // {image: 'assets/img/home/jobs3.jpg', description: 'Image 02'},
+                    // {image: 'assets/img/home/jobs5.jpg', description: 'Image 04'}
+
+                    {image: 'assets/img/home/img00.jpg', description: 'Image 00'},
+                    {image: 'assets/img/home/img01.jpg', description: 'Image 01'},
+                    {image: 'assets/img/home/img02.jpg', description: 'Image 02'},
+                    {image: 'assets/img/home/img03.jpg', description: 'Image 04'},
+                    {image: 'assets/img/home/img04.jpg', description: 'Image 04'}
                 ];
 
                 $scope.direction = 'left';
