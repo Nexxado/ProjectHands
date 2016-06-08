04bbf467748430987ced4b5b5157e67d327b51e6
ihabZhaika <zhaika.ihab@gmail.com>
2016-06-08 19:19:39 +0300

    fixed object was not converted to json when send to server

--- client/modules/statistics/services/statistics.js
+++ client/modules/statistics/services/statistics.js
@@ -4,48 +4,54 @@ angular.module('ProjectHands.statistics')
         var baseUrl = '/api/statistics';
 
         function getVolunteersCountPerDate(year, month, dayFrom, dayTo) {
-            return $resource(baseUrl + '/VolunteersCountPerDate').get({
+            var date = {
                 year: year,
                 month: month,
                 dayFrom: dayFrom,
                 dayTo: dayTo
-            });
+            };
+            return $resource(baseUrl + '/VolunteersCountPerDate').get({date: JSON.stringify(date)});
         }
 
         function getRenovationsVolunteersNumberPerDate(year, month, dayFrom, dayTo) {
-            return $resource(baseUrl + '/renovationsVolunteersNumberPerDate').get({
+            var date = {
                 year: year,
                 month: month,
                 dayFrom: dayFrom,
                 dayTo: dayTo
-            });
+            };
+            return $resource(baseUrl + '/renovationsVolunteersNumberPerDate').get({date: JSON.stringify(date)});
         }
 
         function getRenovationsCostPerDate(year, month, dayFrom, dayTo) {
-            return $resource(baseUrl + '/renovationsCostPerDate').get({
+            var date = {
                 year: year,
                 month: month,
                 dayFrom: dayFrom,
                 dayTo: dayTo
-            });
+            };
+            return $resource(baseUrl + '/renovationsCostPerDate').get({date: JSON.stringify(date)});
         }
 
         function getRenovationsVolunteeringHoursPerDate(year, month, dayFrom, dayTo) {
-            return $resource(baseUrl + '/renovationsVolunteeringHoursPerDate').get({
+            var date = {
                 year: year,
                 month: month,
                 dayFrom: dayFrom,
                 dayTo: dayTo
-            });
+            };
+            return $resource(baseUrl + '/renovationsVolunteeringHoursPerDate').get({date: JSON.stringify(date)});
         }
 
         function getRenovationsPerDate(year, month, dayFrom, dayTo) {
-            return $resource(baseUrl + '/renovationsPerDate').get({
+            var date = {
                 year: year,
                 month: month,
                 dayFrom: dayFrom,
                 dayTo: dayTo
-            });
+            };
+            
+            return $resource(baseUrl + '/renovationsPerDate').get({date: JSON.stringify(date)});
         }
 
         return {
