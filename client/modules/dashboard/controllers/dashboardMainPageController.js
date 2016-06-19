aee6a632f2b89b525defb28059aa3bde86eed38b
Netanel Draiman <ndraiman@gmail.com>
2016-06-19 23:43:38 +0300

    fixed responsive layout for all-teams, join-requests and main page of dashboard

--- client/modules/dashboard/controllers/dashboardMainPageController.js
+++ client/modules/dashboard/controllers/dashboardMainPageController.js
@@ -2,5 +2,32 @@ angular.module('ProjectHands.dashboard')
 
 .controller('DashboardMainPageController', function ($scope) {
 
-	
+	$scope.dashboardLinks = [
+        {
+            state: 'dashboard.all-teams',
+            title: 'צוותים',
+            desc: 'כל הצוותים של פרויקט ידיים',
+            img: 'Teams.png'
+        } , {
+            state: 'dashboard.renovations',
+            title: 'שיפוצים מתוכננים',
+            desc: 'כל השיפוצים שמתוכננים בעתיד',
+            img: 'Renovations.png'
+        } , {
+            state: 'dashboard.calendar',
+            title: 'לוח שנה',
+            desc: 'לוח שנה שמציג את כל האירועים הקרובים',
+            img: 'Calendar.png'
+        } , {
+            state: 'dashboard.statistics',
+            title: 'סטטיסטיקות',
+            desc: 'סטטיסטיקות של כל הפרויקטים והאנשים של פרויקט ידיים',
+            img: 'Statistics.png'
+        } , {
+            state: 'dashboard.join-requests',
+            title: 'בקשות הצטרפות',
+            desc: 'כל האנשים שנרשמו וצריכים לקבל אישור הצטרפות',
+            img: 'NewMembers.png'
+        }
+    ]
 });
